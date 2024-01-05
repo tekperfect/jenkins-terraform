aa2281972b48ea98696cc530ad3c0654c3d89e1f
alfonsoh <alfonso@tekperfect.com>
2024-01-04 21:29:39 -0800

    updating code for deployment

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
-resource "aws_instance" "public_instance" {
- ami           = var.ami
- instance_type = var.instance_type
+resource "aws_key_pair" "autodeploy" {
+  key_name   = "autodeploy"  # Set a unique name for your key pair
+  public_key = file("/var/jenkins_home/.ssh/id_rsa.pub")
+}
 
- tags = {
-   Name = var.name_tag,
- }
+resource "aws_instance" "public_instance" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.name_tag,
+  }
+  
+  key_name = aws_key_pair.autodeploy.key_name  # Link the key pair to the instance
 }

6b05167a5034e80908b8ab33ba4065f225006458
Rodrick "roddybo" Saunders <146304638+roddybo@users.noreply.github.com>
2024-01-11 10:56:57 -0800

    Update allow_ssh.tf

--- allow_ssh.tf
+++ allow_ssh.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     
     # Replace the following with your specific allowed IP addresses
-    cidr_blocks = ["10.0.0.9", "10.0.0.64"]
+    cidr_blocks = ["10.0.0.9/24", "10.0.0.64/24", "192.168.0.67/24"]
   }
 
   # Add any additional rules or configurations here

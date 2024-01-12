b18d6c24388a88bf9305042dce80d08655d99838
roddybo <roddybo@gmail.com>
2024-01-12 01:16:56 -0800

    Merge changes from remote

--- allow_ssh.tf
+++ allow_ssh.tf
@@ -1,7 +1,7 @@
-resource "aws_security_group" "Super_Admin" {
-       name        = "Super_Admin"
-       description = "Some description"
-       vpc_id      = "${aws_vpc.my_vpc.id}"
+resource "aws_security_group" "secure ssh" {
+       name        = "secure ssh"
+       description = "allow ssh from my home ip"
+       vpc_id      = "${aws_vpc.vpc-02ef769587dd081cf }"
        tags {
          Name = "my_sg_tag"
        }

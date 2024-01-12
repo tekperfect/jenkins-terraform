9dd88f09713e041ce7a78da1a54ffc7ad1f752cb
roddybo <roddybo@gmail.com>
2024-01-12 00:07:04 -0800

    Merge changes from remote

--- allow_ssh.tf
+++ allow_ssh.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "my_sg" {
          from_port   = "22"
          to_port     = "22"
          protocol    = "TCP"
-         cidr_blocks = ["73.116.216.200/32","76.102.161.122,"98.45.139.47"]
+         cidr_blocks = ["73.116.216.200/32","76.102.161.122","98.45.139.47"]
        }
 
        # Outbound Access    

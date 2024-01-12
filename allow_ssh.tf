28d331e3c845824caadb1d0f6557422e7423c856
roddybo <roddybo@gmail.com>
2024-01-12 02:00:58 -0800

    Merge changes from remote

--- allow_ssh.tf
+++ allow_ssh.tf
@@ -1,42 +1,32 @@
-resource "aws_security_group" "my_sg" {
-       name        = "my_sg"
-       description = "Some description"
-       vpc_id      = "${aws_vpc.my_vpc.id}"
-       tags {
-         Name = "my_sg_tag"
-       }
-
-       #Not redundant - Inbound Access
-       ingress {
-         from_port   = "22"
-         to_port     = "22"
-         protocol    = "TCP"
-         cidr_blocks = ["73.116.216.200"/32","76.102.161.122,"98.45.139.47"]
-       }
-
-       # Outbound Access    
-       egress {
-         from_port   = 0
-         to_port     = 0
-         protocol    = "-1"
-         cidr_blocks = ["0.0.0.0/0"]
-       }
+# Define a variable for the CIDR block of your allowed IP address
+variable "allowed_ip" {
+  type    = string
+  default = "73.116.216.200"
 }
 
+resource "aws_security_group" "my_sg" {
+  name        = "my_sg"
+  description = "Your description here"
+  vpc_id      = vpc-02ef769587dd081cf   # Replace with your VPC resource name or ID
 
+  tags = {
+    Name = "my_sg_tag"
+  }
 
+  # Inbound Access
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ip]
+  }
 
-
-GD
-T
-16 external people are from 15 organizations
-
-
-
-
-
-
-
-
-
+  # Outbound Access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 

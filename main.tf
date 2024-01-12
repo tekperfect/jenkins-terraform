7b797f83c959738e4c8eb8474dd967aa59805fa7
Rodrick "roddybo" Saunders <146304638+roddybo@users.noreply.github.com>
2024-01-12 09:25:49 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,26 @@
 resource "aws_key_pair" "autodeploy" {
-#  key_name   = "autodeploy"  # Set a unique name for your key pair
   public_key = file("/var/jenkins_home/.ssh/id_rsa.pub")
 }
 
 resource "aws_instance" "public_instance" {
   ami           = var.ami
   instance_type = var.instance_type
+  key_name      = aws_key_pair.autodeploy.key_name
+  
+  // Other instance configurations...
+
+  // Attach the primary EBS volume (default size)
+  root_block_device {
+    volume_size = 8  # Adjust the size based on your needs
+  }
+
+  // Attach an additional EBS volume (2 GB)
+  ebs_block_device {
+    device_name = "/dev/sdb"
+    volume_size = 2
+    volume_type = "gp2"  # Adjust volume type as needed
+  }
 
   tags = {
     Name = var.name_tag,
   }
-  
-  key_name = aws_key_pair.autodeploy.key_name  # Link the key pair to the instance
-}

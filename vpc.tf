2ceff7e55ff36f6cc19d32580df821d78d403795
Rodrick "roddybo" Saunders <146304638+roddybo@users.noreply.github.com>
2024-01-13 03:36:34 -0800

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,1 +1,14 @@
-
+# Setting up VPC
+//network.tf
+resource "aws_vpc" "blueteam" {
+  cidr_block = "10.0.0.0/24"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  }
+# Setting up the subnets
+//subnets.tf
+resource "aws_subnet" "subnet-uno" {
+  cidr_block = "${cidrsubnet(aws_vpc.blueteam.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.blueteam.id}"
+  availability_zone = "us-east-1a"
+}
